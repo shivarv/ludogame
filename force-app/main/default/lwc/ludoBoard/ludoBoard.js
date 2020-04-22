9343f64ef7f4d578bf31c36bf9b52d2e8fff5a1d
shiva <shivarv1993@gmail.com>
2020-04-22 21:13:30 +0100

    basic lwc creation

---
+++ force-app/main/default/lwc/ludoBoard/ludoBoard.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class LudoBoard extends LightningElement {
+
+    playerName;
+    playerType;
+    playerList = ['player1', 'player2', 'player3', 'player4'];
+    playerCount;
+}
---
+++ force-app/main/default/lwc/ludoMiddlePath/ludoMiddlePath.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class LudoMiddlePath extends LightningElement {}
