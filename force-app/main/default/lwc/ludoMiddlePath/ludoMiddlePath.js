2538fa993c3e54e49d594d50946eee4d111ce0ad
shiva <shivarv1993@gmail.com>
2020-04-26 14:40:56 +0100

    css

--- force-app/main/default/lwc/ludoMiddlePath/ludoMiddlePath.js
+++ force-app/main/default/lwc/ludoMiddlePath/ludoMiddlePath.js
@@ -1,5 +1,11 @@
 import { LightningElement, api } from 'lwc';
 
 export default class LudoMiddlePath extends LightningElement {
-    @api isAllowed;
+    @api isHorizontal;
+    count  = 24;
+    
+    // width: 5.83vw;height: 8vh;background-color:green;position: absolute;left:5.83vw;
+    constructor() {
+        super();
+    }
 }
